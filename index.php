cf1451aad9d5a84cf5db19c70780acc6eef26451
thanos <thanosdoumas555@gmail.com>
2021-10-08 17:35:36 +0300

    Alpha init

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Twitter clone</title>
+</head>
+<body>
+    <header>
+        
+
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.js"/>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="assets/css/style.css"/>
+    </footer>
+</body>
+</html>
